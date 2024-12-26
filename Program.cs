a696ecabcca8396471303fe5416026ca136fbf05
نورة <83267402+Norah22-22@users.noreply.github.com>
2024-12-27 00:03:45 +0300

    good

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class User
+{
+    public string Name { get; set; }
+    public int ID { get; private set; }
+    private static int totalUsers = 0;
+    private static int nextID = 1;
+
+    public User(string name)
+    {
+        totalUsers++;
+        this.ID = nextID++;
+        this.Name = string.IsNullOrEmpty(name) ? "No Name Provided" : name;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {Name}, ID: {ID}");
+    }
+
+    public static void DisplayTotalUsers()
+    {
+        Console.WriteLine($"Total Users: {totalUsers}");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        User user1 = new User("Norah");
+        User user2 = new User("Sara");
+        User user3 = new User(null);
+
+        user1.DisplayInfo();
+        user2.DisplayInfo();
+        user3.DisplayInfo();
+
+        User.DisplayTotalUsers();
+
+        User? userNull = null;
+        if (userNull != null)
+        {
+            userNull.DisplayInfo();
+        }
+        else
+        {
+            Console.WriteLine("Null");
+        }
+    }
+}
+
